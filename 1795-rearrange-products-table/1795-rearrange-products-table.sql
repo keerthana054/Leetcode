739152a06bab29023053f4e2b1e4f5705a3bacf4
Keerthana Sathyakumar <91015385+keerthana054@users.noreply.github.com>
2023-12-28 14:24:41 +0530

    Time: 1518 ms (6.79%), Space: 0B (100.00%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -1,11 +1,16 @@
-SELECT product_id, 'store1' AS store, store1 AS price
-FROM Products
-WHERE store1 IS NOT NULL
-UNION ALL
-SELECT product_id, 'store2' AS store, store2 AS price
-FROM Products
-WHERE store2 IS NOT NULL
-UNION ALL
-SELECT product_id, 'store3' AS store, store3 AS price
-FROM Products
-WHERE store3 IS NOT NULL
+SELECT *
+FROM (
+    SELECT product_id, 'store1' AS store, store1 AS price 
+    FROM Products
+
+    UNION
+
+    SELECT product_id, 'store2' AS store, store2 AS price
+    FROM Products
+
+    UNION
+
+    SELECT product_id, 'store3' AS store, store3 AS price
+    FROM Products
+) AS t
+WHERE price IS NOT NULL;
