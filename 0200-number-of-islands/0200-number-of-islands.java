9b68942729d7f1339ba6fa2a163e66d5b12ac3d6
Keerthana Sathyakumar <91015385+keerthana054@users.noreply.github.com>
2024-01-06 09:50:37 +0530

    Time: 3 ms (78.28%), Space: 49.4 MB (33.13%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,40 +1,24 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0) {
-            return 0;
-        }
-        
-        int rows = grid.length;
-        int cols = grid[0].length;
         int count = 0;
-        
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == '1') {
-                    count++;
-                    dfs(grid, i, j);
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    count+=1;
+                    callBFS(grid,i,j);
                 }
-            }
+                }
         }
-        
         return count;
     }
-    
-    private void dfs(char[][] grid, int row, int col) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        
-        if (row < 0 || row >= rows || col < 0 || col >= cols || grid[row][col] == '0') {
+    public void callBFS(char[][] grid, int i, int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[i].length || grid[i][j]=='0'){
             return;
         }
-        
-        // Mark the cell as visited
-        grid[row][col] = '0';
-        
-        // Explore neighbors
-        dfs(grid, row - 1, col);
-        dfs(grid, row + 1, col);
-        dfs(grid, row, col - 1);
-        dfs(grid, row, col + 1);
+        grid[i][j]='0';
+        callBFS(grid,i+1,j);
+        callBFS(grid,i-1,j);
+        callBFS(grid,i,j+1);
+        callBFS(grid,i,j-1);
     }
 }
